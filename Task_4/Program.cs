2b774b618160eb2eda99b674f633b226b693dacb
Liza <kuznecovaliza11@yandex.ru>
2023-02-12 17:21:35 +0700

    Задача 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите три числа:");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int Max = Convert.ToInt32(Console.ReadLine());
+ 
+ if (A >= Max)
+ {
+Max = A;
+ }
+ if (B >= Max)
+ {
+    Max = B;
+ }
+
+Console.WriteLine($"Max: {Max}");
