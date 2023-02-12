c725e00565c7738a74061601a7756eb06267f3d7
Liza <kuznecovaliza11@yandex.ru>
2023-02-12 17:52:55 +0700

    Задача 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите одно число:");
+int A = Convert.ToInt32(Console.ReadLine());
+
+ if (A % 2 == 0)
+ {
+Console.WriteLine("ДА");
+ }
+  else
+ {
+  Console.WriteLine("НЕТ");
+ }
+
+
